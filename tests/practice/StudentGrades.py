cc4d3e6a6f673d7530ffc3a3338a622a7c586005
Jesdev <jestalycastillo15@gmail.com>
2025-07-01 19:25:21 +0800

    Add: practice problem

---
+++ tests/practice/StudentGrades.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+def create_dataframe():
+    df = pd.DataFrame({
+        'Name':['John','Murphy','Ann','Cassy','Justin'],
+        'Math':[88,94,92,85,96],
+        'Science':[90,90,91,92,91],
+        'English':[98,92,90,92,90]
+        })
+    return df
+
+def get_first_row():
+    df = create_dataframe()
+    
+    #first_five_rows = df.head()
+    first_row = df.iloc[0,:]
+    print(first_row)
+
+def create_column():
+    df = create_dataframe()
+
+    average_list = []
+    for i in range(4 - 1):
+
+        average_list[i] = df.iloc[i,]
+
+    df['Average'] = average_list
+
+    print(df)
+
+create_column()
