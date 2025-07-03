986e9ef4dc6008b45d7436f2a999a647f8c7ce12
Jesdev <jestalycastillo15@gmail.com>
2025-07-03 11:55:23 +0800

    Add: practice

---
+++ tests/practice/StudentPerformanceTracker.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+def create_dataframe():
+    df = pd.DataFrame(
+            {
+                'Name':['John','Justin','Eric','Ann','Mary'],
+                'Math':[90,88,96,85,88],
+                'Science':[90,90,89,92,94],
+                'English':[94,96,96,92,93]
+                }
+            )
+
+    return df
+
+def show_first_three_student_scores():
+    df = create_dataframe()
+
+    scores = df.iloc[:3,:]
+    print(scores)
+
+def show_science_score():
+    df = create_dataframe()
+
+    person_record = df.loc[df.Name == 'Ann']
+    science_score = person_record['Science']
+    print(science_score)
+
+def change_score():
+    df = create_dataframe()
+
+    df.iloc[1,1] = 95
+
+    print(df)
+
+def change_english_score():
+    df = create_dataframe()
+
+    df.loc[df.Name == 'Mary', 'English'] = 88
+
+    print(df)
+
+def create_column():
+    df = create_dataframe()
+
+    df.loc[]
+
+    print(df)
+
+print(create_dataframe())
+print()
+show_first_three_student_scores()
+print()
+show_science_score()
+print()
+change_score()
+print()
+change_english_score()
+print()
+create_column()
